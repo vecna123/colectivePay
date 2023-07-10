735b9efd04a01082584b752aefa1dee098f4b74b
Josué Vera <josue.vera@efby.cl>
2023-07-10 13:22:59 -0400

    .

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,21 +9,6 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () =>
-          import('../tab1/tab1.module').then((m) => m.Tab1PageModule),
-      },
-      {
-        path: 'tab2',
-        loadChildren: () =>
-          import('../tab2/tab2.module').then((m) => m.Tab2PageModule),
-      },
-      {
-        path: 'tab3',
-        loadChildren: () =>
-          import('../tab3/tab3.module').then((m) => m.Tab3PageModule),
-      },
-      {
         path: 'billetera',
         loadChildren: () => import('../billetera/billetera.module').then( m => m.BilleteraPageModule)
       },
@@ -33,7 +18,7 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/billetera',
         pathMatch: 'full',
       },
     ],
